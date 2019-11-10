c17707642edc444a49edd11a5fcf230bcc35e0cb
ahmarulabdi <rndmjck@gmai.com>
2019-11-10 16:07:57 +0700

    first comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+  `
+  <h1>Tour of Heroes</h1>
+  <h2>My favorite hero is : {{ hero }}</h2>
+  `
 })
 export class AppComponent {
   title = 'displaying-data';
+  hero = 'Windostroms';
 }
