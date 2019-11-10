fb3a647176001e575d62592624e5c7e19e02d662
ahmarulabdi <rndmjck@gmai.com>
2019-11-10 16:19:13 +0700

    displaying simple array data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,13 @@ import { Component } from '@angular/core';
   `
   <h1>Tour of Heroes</h1>
   <h2>My favorite hero is : {{ hero }}</h2>
+  <p>heroes</p>
+  <ul>
+  <li *ngFor="let hero of heroes">{{ hero }}</li>
+  </ul>
   `
 })
 export class AppComponent {
   title = 'displaying-data';
-  hero = 'Windostroms';
+  heroes = ['Windostroms','Hulk', 'IronMan', 'Thor', 'Superman', 'Batman'];
 }
